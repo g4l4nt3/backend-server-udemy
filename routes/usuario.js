ea577a5632faeda26c1d3c39d0c006493f7069a5
Diego Galante <diego.galante@tipsalud.local>
2018-08-02 15:11:52 -0300

    metodos de actualizacion(PUT) y borrado (DELETE) de usuario

--- routes/usuario.js
+++ routes/usuario.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var app = express();
 var Usuario = require('../models/usuario');
+var bcrypt = require('bcryptjs');
 
-
+//==================================================
 // Obtener todos los usuarios
+//==================================================
 app.get('/', (request, response, next) => {
 
     Usuario.find({}, 'nombre email img role').exec(
@@ -24,14 +26,65 @@ app.get('/', (request, response, next) => {
         });
 });
 
+//==================================================
+// Actualizar usuario
+//==================================================
+app.put('/:id', (request, response) => {
+    var id = request.params.id;
+    var body = request.body;
+
+    Usuario.findById(id, 'nombre email img role').exec((err, usuario) => {
+
+        if (err) {
+            return response.status(500).json({
+                ok: false,
+                mensaje: 'Error al buscar usuario',
+                errors: err
+            });
+        }
+
+        if (!usuario) {
+            return response.status(400).json({
+                ok: false,
+                mensaje: 'El usuario con el id ' + id + ' no existe',
+                errors: { message: 'No existe un usuario con ese ID' }
+            });
+        }
+
+        usuario.nombre = body.nombre;
+        usuario.email = body.email;
+        if (body.role) usuario.role = body.role;
+
+        usuario.save((err, usuarioGuardado) => {
+            if (err) {
+                return response.status(400).json({
+                    ok: false,
+                    mensaje: 'Error al actualizar usuario',
+                    errors: err
+                });
+            }
+
+            response.status(200).json({
+                ok: true,
+                usuario: usuarioGuardado
+            });
+        });
+    });
+});
+
+//==================================================
 // Crear un nuevo usuario
+//==================================================
 app.post('/', (request, response) => {
     var body = request.body;
+
+    let encriptarPass = (password) =>
+        (password) ? bcrypt.hashSync(password, 10) : null;
 
     var usuario = new Usuario({
         nombre: body.nombre,
         email: body.email,
-        password: body.password,
+        password: encriptarPass(body.password),
         img: body.img,
         role: body.role
     });
@@ -53,6 +106,30 @@ app.post('/', (request, response) => {
     });
 
 });
+//==================================================
+// Actualizar usuario
+//==================================================
+app.delete('/:id', (request, response) => {
+    var id = request.params.id;
 
+    Usuario.findByIdAndRemove(id, (err, usuarioBorrado) => {
+
+        if (err) {
+            return response.status(500).json({
+                ok: false,
+                mensaje: 'Error al borrar usuario',
+                errors: err
+            });
+        }
+
+        response.status(201).json({
+            ok: true,
+            usuario: usuarioBorrado
+        });
+
+    });
+
+
+});
 
 module.exports = app;
