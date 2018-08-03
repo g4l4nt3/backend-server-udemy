9ce74aebaf23ff926c6a52257bb439e0e295f9bc
Diego Galante <diego.galante@tipsalud.local>
2018-08-03 14:41:12 -0300

    Busqueda por coleccion parametrizada

--- routes/busqueda.js
+++ routes/busqueda.js
@@ -4,6 +4,9 @@ var Hospital = require('../models/hospital');
 var Medico = require('../models/medico');
 var Usuario = require('../models/usuario');
 
+//==================================================
+// Busqueda general
+//==================================================
 app.get('/todo/:busqueda', (request, response, next) => {
     var busqueda = request.params.busqueda;
     var regex = new RegExp(busqueda, 'i');
@@ -25,6 +28,7 @@ app.get('/todo/:busqueda', (request, response, next) => {
 function buscarHospitales(regex) {
     return new Promise((resolve, reject) => {
         Hospital.find({ nombre: regex })
+            .populate('usuario', 'nombre email')
             .exec((err, hospitales) => {
                 if (err)
                     reject({ menssage: 'Error al cargar hospitales' });
@@ -37,6 +41,8 @@ function buscarHospitales(regex) {
 function buscarMedicos(regex) {
     return new Promise((resolve, reject) => {
         Medico.find({ nombre: regex })
+            .populate('usuario', 'nombre email')
+            .populate('hospital', 'nombre')
             .exec((err, medicos) => {
                 if (err)
                     reject({ menssage: 'Error al cargar medicos' });
@@ -48,7 +54,7 @@ function buscarMedicos(regex) {
 
 function buscarUsuarios(regex) {
     return new Promise((resolve, reject) => {
-        Usuario.find({})
+        Usuario.find({}, 'nombre email')
             .or([{ nombre: regex }, { email: regex }]) //busqueda por dos campos
             .exec((err, usuarios) => {
                 if (err)
@@ -58,5 +64,42 @@ function buscarUsuarios(regex) {
             });
     });
 }
+
+//==================================================
+// Busqueda por coleccion
+//==================================================
+app.get('/coleccion/:tabla/:busqueda', (request, response) => {
+    var busqueda = request.params.busqueda;
+    var tabla = request.params.tabla;
+    var regex = new RegExp(busqueda, 'i');
+    var promesa;
+
+    switch (tabla) {
+        case 'hospital':
+            promesa = buscarHospitales(regex);
+            break;
+
+        case 'medico':
+            promesa = buscarMedicos(regex);
+            break;
+
+        case 'usuario':
+            promesa = buscarUsuarios(regex);
+            break;
+
+        default:
+            return response.status(400).json({
+                ok: false,
+                mensaje: 'La coleccion solo puede ser usuario, hospital o medico'
+            });
+    }
+
+    promesa.then(data => {
+        return response.status(200).json({
+            ok: true,
+            [tabla]: data //Indica que la propiedad es la variable!
+        });
+    });
+});
 
 module.exports = app;
