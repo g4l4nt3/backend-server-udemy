a67cea7f24e3fbf36557defc9cb7caa4acc44be4
Diego Galante <diego.galante@tipsalud.local>
2018-08-06 12:13:27 -0300

    Obtener imagenes por petición

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ var hospitalRoutes = require('./routes/hospital');
 var medicoRoutes = require('./routes/medico');
 var busquedasRoutes = require('./routes/busqueda');
 var uploadRoutes = require('./routes/upload');
+var imagenesRoutes = require('./routes/imagenes');
 
 
 // Conexion a la base de datos
@@ -30,6 +31,11 @@ mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB',
         console.log('Base de datos hospitalDB: \x1b[32m%s\x1b[0m', 'online');
     });
 
+// Server index config - npm install serve-index
+// var serveIndex = require('serve-index');
+// app.use(express.static(__dirname + '/'))
+// app.use('/uploads', serveIndex(__dirname + '/uploads'));
+
 
 // Rutas
 app.use('/usuario', usuarioRoutes);
@@ -38,6 +44,7 @@ app.use('/hospital', hospitalRoutes);
 app.use('/medico', medicoRoutes);
 app.use('/busqueda', busquedasRoutes);
 app.use('/upload', uploadRoutes);
+app.use('/img', imagenesRoutes);
 app.use('/', appRoutes);
 
 // Escuchar peticiones
---
+++ routes/imagenes.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+const path = require('path');
+const fs = require('fs');
+
+app.get('/:tipo/:img', (request, response) => {
+    var tipo = request.params.tipo;
+    var img = request.params.img;
+
+    var pathImagen = path.resolve(__dirname, `../uploads/${ tipo }/${ img }`);
+    if (fs.existsSync(pathImagen)) {
+        response.sendFile(pathImagen);
+    } else {
+        var pathNoImage = path.resolve(__dirname, `../assets/no-img.jpg`);
+        response.sendFile(pathNoImage);
+    }
+});
+
+module.exports = app;
