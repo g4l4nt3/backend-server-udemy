359f87b9d891157b92abb404782239ed99ee5c45
Diego Galante <diego.galante@tipsalud.local>
2018-08-14 15:25:15 -0300

    login - CORS

--- app.js
+++ app.js
@@ -6,6 +6,15 @@ var bodyParser = require('body-parser');
 // Inicializar variables
 var app = express();
 
+// CORS
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
+    next();
+});
+
 
 //Body Parser: permite hacer un parseo de los parametros de request application/json y application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
